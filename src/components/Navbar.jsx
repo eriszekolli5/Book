ccc52f5ac2833404bc7cf2ea1b1713afdcc14154
eriszekolli5 <eriszekolli5@gmail.com>
2026-06-10 02:16:58 +0200

    login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
 import React, { useEffect, useState } from "react"
 import { Link, NavLink } from 'react-router-dom'
+import { baseUrl, logOutUser } from "../services/auth";
 
 function Navbar() {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const userId = localStorage.getItem("userId");
+        console.log(userId, 'userId');
+
+        if (userId) {
+            fetch(`${baseUrl}/users/${userId}`)
+                .then((response) => response.json())
+                .then((data) => setUser(data))
+        }
+
+    }, [])
+
+
+    const handleLogout = () => {
+        logOutUser();
+        window.location.href("/");
+    }
     return (
         <header>
             <nav className='shadow-sm sticky top-0 z-50 bg-white'>
@@ -28,23 +47,44 @@ function Navbar() {
                             <NavLink to="/contact">Contact</NavLink>
                         </li>
 
-                        <li>
-              <NavLink
-                to="/login"
-                className="bg-purple-800 text-white px-4 py-2 rounded-sm"
-              >
-                Login
-              </NavLink>
-            </li>
+                        {!user && (
+                            <>
 
-            <li>
-              <NavLink
-                to="/register"
-                className="bg-purple-800 text-white px-4 py-2 rounded-sm"
-              >
-                Get Started
-              </NavLink>
-            </li>
+                                <li>
+                                    <NavLink
+                                        to="/login"
+                                        className="bg-purple-800 text-white px-4 py-2 rounded-sm"
+                                    >
+                                        Login
+                                    </NavLink>
+                                </li>
+
+                                <li>
+                                    <NavLink
+                                        to="/register"
+                                        className="bg-purple-800 text-white px-4 py-2 rounded-sm"
+                                    >
+                                        Get Started
+                                    </NavLink>
+                                </li>
+
+                            </>
+                        )}
+
+                        {user && (
+
+                            <>
+                            <li>
+                                Hi, {user.name}
+                            </li>
+                                <li>
+                                    <NavLink onClick={handleLogout} className="bg-red-700 text-blue-50 px-4 py-2 rounded-sm" to="/logout">Logout</NavLink>
+                                </li>
+
+                            </>
+                        )}
+
+
                     </ul>
 
                 </div>
