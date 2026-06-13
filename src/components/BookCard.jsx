ca51069a9f8b5c8b816615cb04bbee46813df731
eriszekolli5 <eriszekolli5@gmail.com>
2026-06-13 02:22:12 +0200

    books

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -5,12 +5,12 @@ function BookCard({ book }) {
   return (
     <Link
       to={`/books/${book.id}`}
-      className="block bg-white rounded-xl shadow-lg border border-gray-200 overflow-hidden hover:-translate-y-2 transition"
+      className="block bg-white rounded-xl shadow-lg border border-gray-200 overflow-hidden hover:-translate-y-2 transition mx-auto w-[320px] h-[480px]"
     >
       <img
         src={book.image}
         alt={book.title}
-        className="w-full h-72 object-cover"
+        className="w-[300px] h-[350px] object-cover rounded mx-auto mt-2"
       />
 
       <div className="p-4">
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -12,12 +12,12 @@ function Books() {
   }, []);
 
   return (
-    <main className="px-100 py-10">
+    <main className=" py-10">
       <h1 className="text-3xl font-bold text-center mb-10">
         Books
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mx-50">
         {books.map((book) => (
           <BookCard key={book.id} book={book} />
         ))}
